39f293fe5a413e960509ac5d260cfdf0a9aa548d
Dương Phạm <pdtduong03@gmail.com>
2021-10-08 10:17:00 +0700

    Add project files.

---
+++ Pratice.CSharp.UploadImage/Models/ImageDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pratice.CSharp.UploadImage.Models
+{
+    public class ImageDbContext : DbContext
+    {
+        public ImageDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<ImageModel> Images { get; set; }
+    }
+}
